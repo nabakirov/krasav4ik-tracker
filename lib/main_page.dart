b73a372db2d6d325c434e29829c7dac57531f00d
Nurs <nabakirov@gmail.com>
2019-08-08 10:48:19 +0600

    add secure storage

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:web_socket_channel/io.dart';
 import 'pages/home_page.dart';
 import 'pages/settings_page.dart';
@@ -28,12 +29,16 @@ class _MainPageState extends State<MainPage> {
   String rpcUrl = 'http://ropsten.fridayte.ch';
   String wsUrl = 'ws://ropsten.fridayte.ch:5001';
   EthereumAddress contractAddress = EthereumAddress.fromHex('0xb012241f80f77a728b144a50be2c3f8135bd7ca8');
-  String userPrivateKey = '';
+  String userPrivateKey;
+  // String userPrivateKey = '9857c0274f686e96653680cd176efa3001ef670567cc65ff91862e966d40ba19';
   Credentials userCredentials;
   Client httpClient;
   Web3Client ethClient;
   File abiFile;
   DeployedContract contract;
+  bool isUserPrivateKeyInvalid = false;
+
+  final storage = new FlutterSecureStorage();
 
 
   bool isLoading = false;
@@ -94,35 +99,114 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void init() async {
-    
-    var _contract = DeployedContract(ContractAbi.fromJson(abiJsonString, 'MetaCoin'), contractAddress);
-    isStartedLoading = true;
-    isLoading = true;
+  void _initPrivateKey() async {
+    try{
+      userCredentials = await ethClient.credentialsFromPrivateKey(userPrivateKey);
+      isLoading = false;
+      await storage.write(key: 'secretKey', value: userPrivateKey);
+    } on FormatException {
+      isLoading = true;
+      isUserPrivateKeyInvalid = true;
+    }
+  }
+
+  void _initWeb3() async {
+    contract = DeployedContract(ContractAbi.fromJson(abiJsonString, 'MetaCoin'), contractAddress);
     ethClient = new Web3Client(rpcUrl, Client(), socketConnector: () {
       return IOWebSocketChannel.connect(wsUrl).cast<String>();
     });
+  }
 
-    ethClient.credentialsFromPrivateKey(userPrivateKey).then((value){
-      setState(() {
-        userCredentials = value;
-        contract = _contract;
-        isLoading = false;
-      });
+
+  void init() async {
+    isLoading = true;
+    isStartedLoading = true;
+    await _initWeb3();
+    userPrivateKey = await storage.read(key: 'secretKey');
+    if (userPrivateKey != null) {
+      await _initPrivateKey();
+    }
+    setState(() {
+      isLoading = isLoading;
+      isStartedLoading = isStartedLoading;
     });
   }
 
+  Widget privateKeyInput() {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('private key'),
+          TextField(
+            decoration: InputDecoration(
+              errorText: isUserPrivateKeyInvalid ? 'invalid key': null
+            ),
+            onChanged: (text) {
+              
+              setState(() {
+                userPrivateKey = text;
+                isUserPrivateKeyInvalid = false;
+              });
+            },
+            autocorrect: false,
+            autofocus: true,
+            textCapitalization: TextCapitalization.none,
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.grey),
+            maxLines: 5,
+            minLines: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void loginBtnOnPressed() async {
+    await _initPrivateKey();
+
+    setState(() {
+      contract = contract;
+      ethClient = ethClient;
+      isLoading = isLoading;
+      userCredentials = userCredentials;
+    });
+  }
+
+  Widget loginWidget() {
+    return Scaffold(
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Logo(),
+              
+              privateKeyInput(),
+
+              RaisedButton(
+                child: Text('login'),
+                onPressed: () => loginBtnOnPressed(),
+              ),
+            ],
+          ),
+    );
+  }
+
   Widget _pageToDisplay() {
-    print(isStartedLoading);
-    print(isLoading);
     if (isStartedLoading) {
       if (!isLoading) {
         return app();
+      } else {
+        return loginWidget();
       }
     } else {
       init();
     }
-    return _loadingView;
+    return Scaffold(
+      body: Center(child: Logo(),)
+      );
   }
 
   Widget app() {
