f4f30eda89203234aa4b222ecd44327b357e736f
Nurs <nabakirov@gmail.com>
2019-08-22 10:33:47 +0600

    update node urls

--- lib/configs.dart
+++ lib/configs.dart
@@ -2,7 +2,7 @@
 
 final String contractAddressHex = '0xb012241f80f77a728b144a50be2c3f8135bd7ca8';
 final String rpcUrl = 'http://ropsten.fridayte.ch';
-final String wsUrl = 'ws://ropsten.fridayte.ch:5001';
+final String wsUrl = 'ws://wsropsten.fridayte.ch';
 final String etherscan = 'https://ropsten.etherscan.io/tx/';
 
 
