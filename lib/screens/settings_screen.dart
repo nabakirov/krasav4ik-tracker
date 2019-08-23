5e4bb6a2d29cdf5fa5f01318b9cdc35db8069600
Nurs <nabakirov@gmail.com>
2019-08-23 11:19:52 +0600

    update nickname changer to modal widget generator

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:krasav4ik/blocs/blocs.dart';
+import 'package:krasav4ik/tools/modal.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -11,84 +12,74 @@ class SettingsScreen extends StatelessWidget {
     var settingsBloc = BlocProvider.of<SettingsBloc>(context);
     var state = settingsBloc.currentState;
     if (state is NicknameInputState) {
-      var _nicknameController = TextEditingController();
-      _nicknameController.text = state.nickname;
-      return Material(
-          child: Stack(
-        children: <Widget>[
+      return modalWidgetGenerator(
           mainWidget(notificationBloc, appBloc, settingsBloc),
-          Stack(
-            children: <Widget>[
-              Positioned.fill(
-                child: Opacity(
-                  opacity: 0.5,
-                  child: InkWell(
-                      child: Container(
-                        color: Colors.grey,
-                      ),
-                      onTap: () => settingsBloc.dispatch(LoadSettingsScreen())),
-                ),
-              ),
-              Center(
-                  child: Card(
-                color: Colors.white,
-                child: Container(
-                  width: 250,
-                  height: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Center(child: Container(
-                        padding: EdgeInsets.only(top: 15),
-                        child: Text('new nickname', style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold)))),
-                      TextField(
-                        controller: _nicknameController,
-                        autofocus: true,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(color: Colors.grey, fontSize: 20),
-                        maxLines: 1,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text('cancel', style: TextStyle(color: Colors.redAccent),),
-                            onPressed: () =>
-                                settingsBloc.dispatch(LoadSettingsScreen()),
-                          ),
-                          FlatButton(
-                            child: Text('save'),
-                            onPressed: () {
-                              String text = _nicknameController.text;
-                              if (text.isEmpty) {
-                                notificationBloc.dispatch(
-                                    NewError('nickname cannot be empty'));
-                              } else if(text.toLowerCase() == state.nickname.toLowerCase()) {
-                                settingsBloc.dispatch(LoadSettingsScreen());
-                              }
-                              
-                              else {
-                                settingsBloc
-                                    .dispatch(ChangeNickname(nickname: text));
-                              }
-                            },
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-              )),
-            ],
-          )
-        ],
-      ));
+          () => settingsBloc.dispatch(LoadSettingsScreen()),
+          nicknameChangerWidget(state, settingsBloc, notificationBloc));
     }
     if (state is BaseSettingsState) {
       return mainWidget(notificationBloc, appBloc, settingsBloc);
     }
+  }
+
+  Widget nicknameChangerWidget(NicknameInputState state,
+      SettingsBloc settingsBloc, NotificationBloc notificationBloc) {
+    var _nicknameController = TextEditingController();
+    _nicknameController.text = state.nickname;
+    return Card(
+      color: Colors.white,
+      child: Container(
+        width: 250,
+        height: 150,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Center(
+                child: Container(
+                    padding: EdgeInsets.only(top: 15),
+                    child: Text('new nickname',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)))),
+            TextField(
+              controller: _nicknameController,
+              autofocus: true,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey, fontSize: 20),
+              maxLines: 1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                  child: Text(
+                    'cancel',
+                    style: TextStyle(color: Colors.redAccent),
+                  ),
+                  onPressed: () => settingsBloc.dispatch(LoadSettingsScreen()),
+                ),
+                FlatButton(
+                  child: Text('save'),
+                  onPressed: () {
+                    String text = _nicknameController.text;
+                    if (text.isEmpty) {
+                      notificationBloc
+                          .dispatch(NewError('nickname cannot be empty'));
+                    } else if (text.toLowerCase() ==
+                        state.nickname.toLowerCase()) {
+                      settingsBloc.dispatch(LoadSettingsScreen());
+                    } else {
+                      settingsBloc.dispatch(ChangeNickname(nickname: text));
+                    }
+                  },
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   Widget mainWidget(NotificationBloc notificationBloc, AppBloc appBloc,
