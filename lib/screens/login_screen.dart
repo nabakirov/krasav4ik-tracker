c7508155e39d79a915746a267ff6ee884c6904f3
Nurs <nabakirov@gmail.com>
2019-08-26 16:20:11 +0600

    fix login screen to be adaptive

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -16,36 +16,15 @@ class LoginScreen extends StatelessWidget {
     appBloc = BlocProvider.of<AppBloc>(context);
 
     return Scaffold(
-        body: Padding(
-            padding: EdgeInsets.fromLTRB(50, 180, 50, 100),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[Logo(), actionWidgets()],
-            )));
-  }
-
-  Widget actionWidgets() {
-    return Container(
+      body: SafeArea(
         child: Column(
-      children: <Widget>[
-        privateKeyInput(),
-        RaisedButton(
-          child: Text('login'),
-          onPressed: () {
-            String key = _privateKeyController.text;
-            print(key);
-            if (key.isEmpty) {
-              notificationBloc.dispatch(NewError('private key is empty'));
-              return null;
-            } else {
-              return appBloc.dispatch(LoginPress(key));
-            }
-          },
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[Logo(), privateKeyInput()],
         ),
-      ],
-    ));
+      ),
+    );
   }
 
   void _pasteFromClipboard() async {
@@ -59,15 +38,14 @@ class LoginScreen extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.fromLTRB(20, 5, 20, 5),
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Text('private key'),
           TextField(
             controller: _privateKeyController,
             autocorrect: false,
-            autofocus: true,
+            autofocus: false,
             textCapitalization: TextCapitalization.none,
-            textAlign: TextAlign.center,
+            // textAlign: TextAlign.center,
             style: TextStyle(color: Colors.grey),
             maxLines: 5,
             minLines: 1,
@@ -76,6 +54,19 @@ class LoginScreen extends StatelessWidget {
             child: Text('paste'),
             onPressed: _pasteFromClipboard,
           ),
+          RaisedButton(
+            child: Text('login'),
+            onPressed: () {
+              String key = _privateKeyController.text;
+              print(key);
+              if (key.isEmpty) {
+                notificationBloc.dispatch(NewError('private key is empty'));
+                return null;
+              } else {
+                return appBloc.dispatch(LoginPress(key));
+              }
+            },
+          )
         ],
       ),
     );
