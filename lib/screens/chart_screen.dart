d67ba6f8c8909d065320f5099ba91f935b3c486d
Nurs <nabakirov@gmail.com>
2019-08-27 17:21:35 +0600

    update chart screen

--- lib/screens/chart_screen.dart
+++ lib/screens/chart_screen.dart
@@ -15,10 +15,37 @@ class ChartScreen extends StatelessWidget {
       for (int i = 0; i < state.items.length; i++) {
         items.add(_cardBuilder(context, state.items[i], i));
       }
+      var theOneUser;
+      if (state.items.length == 0) {
+        theOneUser = UserModel(
+            points: BigInt.from(0),
+            totalAchieves: BigInt.from(0),
+            address: '0x0',
+            nickname: 'no nickname');
+      } else {
+        theOneUser = state.items[0];
+      }
+      if (items.length != 0) {
+        items.removeAt(0);
+      }
       return RefreshIndicator(
         onRefresh: () => _onRefresh(chartBloc),
-        child: ListView(
-          children: items,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(child: theOne(theOneUser), flex: 1),
+            Expanded(
+              flex: 1,
+              child: Container(
+                // height: 400,
+                child: ListView(
+                  children: items,
+                ),
+              ),
+            )
+          ],
         ),
       );
     }
@@ -37,36 +64,123 @@ class ChartScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              (index + 1).toString(),
-              style: TextStyle(fontSize: 15),
+            Container(
+              child: Text(
+                (index + 1).toString(),
+                style: TextStyle(fontSize: 15),
+              ),
             ),
-            Column(
-              children: <Widget>[
-                Text(
-                  userModel.nickname,
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-                Text(
-                  getShortenAddress(userModel.address),
-                  style: TextStyle(color: Colors.grey[500]),
-                )
-              ],
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    userModel.nickname,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Text(
+                    getShortenAddress(address: userModel.address),
+                    style: TextStyle(color: Colors.grey[500]),
+                  )
+                ],
+              ),
             ),
-            Text(
-              '${userModel.totalAchieves}/${userModel.points}',
-              style: TextStyle(fontSize: 15),
+            Container(
+              child: Text(
+                '${userModel.totalAchieves}/${userModel.points}',
+                style: TextStyle(fontSize: 15),
+              ),
             )
           ],
         ),
       ),
     );
   }
-  
-  String getShortenAddress(String address) {
+
+  String getShortenAddress({String address, int count: 7}) {
     int length = address.length;
-    String shorten =
-        address.substring(0, 7) + '...' + address.substring(length - 7, length);
+    String shorten = address.substring(0, count) +
+        '...' +
+        address.substring(length - count, length);
     return shorten;
+  }
+
+  Widget theOne(UserModel userModel) {
+    List<Widget> icons = [];
+    int boxesCount, titsCount;
+    boxesCount = userModel.totalAchieves.toInt();
+    var pointsCount = userModel.points.toInt();
+    titsCount = pointsCount ~/ 10;
+    for (int i = 0; i < boxesCount; i++) {
+      icons.add(Container(
+        child: _imgBuilder('images/box.png'),
+      ));
+    }
+    for (int i = 0; i < titsCount; i++) {
+      icons.add(Container(
+        child: Icon(Icons.adjust),
+      ));
+    }
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+              child: Container(
+            child: _theOneIcon(),
+            padding: EdgeInsets.only(top: 5),
+          )),
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  userModel.nickname,
+                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                ),
+                Text(getShortenAddress(address: userModel.address, count: 12),
+                    style: TextStyle(color: Colors.grey)),
+                Container(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                            child: Text(
+                          "$boxesCount/$pointsCount",
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        )),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: icons,
+                          ),
+                          height: 20,
+                        )
+                      ]),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _imgBuilder(String path) {
+    return Material(
+      child: Image.asset(path),
+    );
+  }
+
+  Widget _theOneIcon() {
+    final String path = 'images/dumbbell.png';
+    return Container(
+      child: _imgBuilder(path),
+    );
   }
 }
