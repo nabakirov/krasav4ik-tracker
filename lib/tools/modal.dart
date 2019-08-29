088da7163ba67a4545b294a38815209fafa5b193
Nurs <nabakirov@gmail.com>
2019-08-29 13:35:31 +0600

    modal generator update
    add name for ok and cancel
    fix bug when body is empty

--- lib/tools/modal.dart
+++ lib/tools/modal.dart
@@ -20,44 +20,58 @@ Widget modalWidgetGenerator(
   ));
 }
 
+Widget _title(String title) {
+  return Center(
+      child: Container(
+          padding: EdgeInsets.only(top: 15),
+          child: Text(title,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold))));
+}
+
+Widget _buttons(
+    String cancelText, Function onCancel, String okText, Function onOk) {
+  return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        FlatButton(
+          child: Text(
+            cancelText,
+            style: TextStyle(color: Colors.redAccent),
+          ),
+          onPressed: onCancel,
+        ),
+        FlatButton(
+          child: Text(okText, style: TextStyle(color: Colors.blueAccent)),
+          onPressed: onOk,
+        )
+      ]);
+}
+
 Widget cardBuilder(
     {@required String title,
     Widget body,
     @required Function onOk,
-    @required Function onCancel}) {
+    @required Function onCancel,
+    String cancelText: 'cancel',
+    String okText: 'save'}) {
+  var children;
+  if (body != null) {
+    children = [
+      _title(title),
+      body,
+      _buttons(cancelText, onCancel, okText, onOk)
+    ];
+  } else {
+    children = [_title(title), SizedBox(height: 20,), _buttons(cancelText, onCancel, okText, onOk)];
+  }
   return Card(
       color: Colors.white,
       child: Container(
-        width: 250,
-        // height: 150,
-        child: Column(
+          width: 250,
+          // height: 150,
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
-            children: [
-              Center(
-                  child: Container(
-                      padding: EdgeInsets.only(top: 15),
-                      child: Text(title,
-                          style: TextStyle(
-                              fontSize: 15, fontWeight: FontWeight.bold)))),
-              body,
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  FlatButton(
-                    child: Text(
-                      'cancel',
-                      style: TextStyle(color: Colors.redAccent),
-                    ),
-                    onPressed: onCancel,
-                  ),
-                  FlatButton(
-                    child: Text('save',
-                        style: TextStyle(color: Colors.blueAccent)),
-                    onPressed: onOk,
-                  )
-                ],
-              )
-            ]),
-      ));
+            children: children,
+          )));
 }
