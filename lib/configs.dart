f3d7854c0f6b7af889f4fbc00db0d1e985709d45
Nurs <nabakirov@gmail.com>
2019-09-02 15:22:57 +0600

    update contract address

--- lib/configs.dart
+++ lib/configs.dart
@@ -1,6 +1,6 @@
 
 
-final String contractAddressHex = '0xabe2e3cb899ffabb787b7e9ab2c97aec6089dab5';
+final String contractAddressHex = '0x2F9D8bd81b9b8B4b86A760D0153c5D03C0841F00';
 final String rpcUrl = 'http://ropsten.fridayte.ch';
 final String wsUrl = 'ws://wsropsten.fridayte.ch';
 final String etherscan = 'https://ropsten.etherscan.io/tx/';
