04a6ba902f4b3859a10495b0583375d60d689c41
Nurs <nabakirov@gmail.com>
2019-09-02 11:24:25 +0600

    change contract address

--- lib/configs.dart
+++ lib/configs.dart
@@ -1,10 +1,10 @@
 
 
-final String contractAddressHex = '0x185c25e20318a958191b168359da36c46740fc2a';
+final String contractAddressHex = '0xabe2e3cb899ffabb787b7e9ab2c97aec6089dab5';
 final String rpcUrl = 'http://ropsten.fridayte.ch';
 final String wsUrl = 'ws://wsropsten.fridayte.ch';
 final String etherscan = 'https://ropsten.etherscan.io/tx/';
 
 
-final int maxGas = 100000;
+final int maxGas = 300000;
 final int gasPriceInWei = 10000000000;
